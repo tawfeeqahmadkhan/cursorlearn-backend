6ea64fdf4014d2f82c2b71e3be765b3130d2549f
pandukayala2-maker <pandukayala2@gmail.com>
2026-03-28 22:44:54 +0530

    all chnages

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
